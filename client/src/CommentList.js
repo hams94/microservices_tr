dc076523b9304ca48743828d6be19cdf08c0a8a7
hams94 <hamidou.bah@altgras.com>
2025-07-27 15:26:42 +0530

    update moderation

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -4,9 +4,23 @@ const CommentList = ({ comments }) => {
   
 
   const renderedComments = Object.values(comments).map((comment) => {
+    let content; 
+
+    if (comment.status  === 'approved'){
+      content = comment.content; 
+    }
+
+    if (comment.status  === 'pending'){
+      content = 'this comment is awaiting moderation '; 
+    }
+
+    if (comment.status  === 'rejected'){
+      content = 'This comment has been rejected '; 
+    }
+
     return (
       <li key={comment.id} >
-        <h3>{comment.content}</h3>
+        {content}
       </li>
     );
   });
