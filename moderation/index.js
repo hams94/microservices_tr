bbd1460e95cd81c70b6ab514f49ec02da9093a1d
hams94 <hamidou.bah@altgras.com>
2025-08-31 17:00:27 +0530

    linking all the services

--- moderation/index.js
+++ moderation/index.js
@@ -6,12 +6,13 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post("/events", async (req, res) => {
+  console.log("Event received ", req.body.type);
   const { type, data } = req.body;
 
   if (type === "CommentCreated") {
     const status = data.content.includes("orange") ? "rejected" : "approved";
 
-    await axios.post("http://localhost:4005/events", {
+    await axios.post("http://event-bus-srv:4005/events", {
       type: "CommentModerated",
       data: {
         id: data.id,
@@ -27,6 +28,7 @@ app.post("/events", async (req, res) => {
 
 app.listen(4003, () => {
   console.log("Listening on 4003");
+  console.log("code updated !");
 });
 
 
